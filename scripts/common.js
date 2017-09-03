434a9fd4d688739be6bfa2bb5ab3349a9e9a961d
Sunlive <sunliveua@gmail.com>
2017-09-03 14:54:07 +0300

    0.1 slider

---
+++ scripts/common.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  const slider = new Swiper('.swiper-container', {
+    loop: true,
+    direction: 'vertical',
+    speed: 1300,
+    autoplay: 1500,
+    effect: 'slide',
+    spaceBetween: 40
+  });
+});
