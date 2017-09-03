f32bc883f9d27bab81c3fb319f3b47eeee322d03
Sunlive <sunliveua@gmail.com>
2017-09-03 15:56:15 +0300

    0.2 timer

--- scripts/common.js
+++ scripts/common.js
@@ -1,10 +1,25 @@
 $(document).ready(() => {
+  // Slider
   const slider = new Swiper('.swiper-container', {
     loop: true,
     direction: 'vertical',
     speed: 1300,
     autoplay: 1500,
     effect: 'slide',
-    spaceBetween: 40
+    spaceBetween: 40,
   });
+
+  // Clock
+  const date = new Date('29 September, 2017 02:15:32');
+  const now = new Date();
+  const difference = (date.getTime() / 1000) - (now.getTime() / 1000);
+  const clockEl = $('.clock');
+
+  const clock = new FlipClock (clockEl,difference, {
+    countdown: true,
+    clockFace: 'DailyCounter',
+    language: 'ru',
+  });
+
+  $('.clock a').click(e => e.preventDefault());
 });
