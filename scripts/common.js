88acdbc39d9448dce5208c952675c69b5b90aa6a
Sunlive <sunliveua@gmail.com>
2017-09-03 19:50:08 +0300

    0.5 some scripts

--- scripts/common.js
+++ scripts/common.js
@@ -12,14 +12,77 @@ $(document).ready(() => {
   // Clock
   const date = new Date('29 September, 2017 02:15:32');
   const now = new Date();
-  const difference = (date.getTime() / 1000) - (now.getTime() / 1000);
+  const difference = date.getTime() / 1000 - now.getTime() / 1000;
   const clockEl = $('.clock');
 
-  const clock = new FlipClock (clockEl, difference, {
+  const clock = new FlipClock(clockEl, difference, {
     countdown: true,
     clockFace: 'DailyCounter',
     language: 'ru',
   });
 
   $('body').click(e => e.preventDefault());
+
+  // scroll to anchor
+  $('a[href^="#"]').on('click', function() {
+    const target = this.hash;
+    const $target = $(target);
+
+    $('html, body').animate(
+      {
+        scrollTop: $target.offset().top,
+      },
+      1500,
+      'swing',
+    );
+  });
+
+  // scroll to top
+  $(window).on('scroll', function() {
+    let offsetY = window.pageYOffset;
+    if (offsetY >= 700) {
+      $('.scrollTop').css({
+        opacity: 1,
+        bottom: 25,
+        visibility: 'visible',
+      });
+    } else {
+      $('.scrollTop').css({
+        opacity: 0,
+        bottom: '-50px',
+        visibility: 'hidden',
+      });
+    }
+  });
+
+  $('.scrollTop').on('click', function() {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      2000,
+    );
+  });
+
+  // form submit
+  $('.submit-button').on('click', function() {
+    //  validation
+    if ($('.name').val() !== '' && $('.phone') !== '' && $('.email') !== '') {
+      const data = `${$('.name').val()} ${$('.phone').val()} ${$(
+        '.email',
+      ).val()}`;
+      $.ajax({
+        type: 'POST',
+        url: 'subMail.php',
+      })
+        .done(function() {
+          alert('thank you');
+        })
+        .fail(function() {
+          alert('error');
+        });
+    } else {
+      return false;
+    }
+  });
 });
